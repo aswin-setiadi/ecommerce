073a8a051ba826221087c60aabce57d94770b91d
aswin <aswin@meshbio.com>
2025-11-03 02:12:35 +0800

    refactor: handle Decimal when producing kafka event

--- order_service/app/kafka_producer.py
+++ order_service/app/kafka_producer.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 import json
 import logging
 import os
@@ -8,6 +9,13 @@ KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS")
 logger = logging.getLogger(__name__)
 producer = None
 
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        # Implement custom serialization logic if needed
+        if isinstance(obj,Decimal):
+            return float(obj)
+        return super().default(obj)
+
 async def get_producer():
     global producer
     if producer is None:
@@ -17,5 +25,10 @@ async def get_producer():
 
 async def produce_event(topic: str, event: dict):
     producer = await get_producer()
-    event_bytes = json.dumps(event).encode('utf-8')
+    try:
+        event_bytes = json.dumps(event, cls= CustomJSONEncoder).encode('utf-8')
+    except TypeError as e:
+        logger.exception(e)
+        raise
     await producer.send_and_wait(topic, event_bytes)
+    logger.info(f"Produced event to topic {topic}: {event}")
